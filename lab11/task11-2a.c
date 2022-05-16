0e1003c9b75892492fa1eb422199a71e0baadbe2
Yana Barbashina <70941492+yanawishnya@users.noreply.github.com>
2022-05-16 17:33:19 +0300

    Lab 11 fixed.

--- lab11/task11-2a.c
+++ lab11/task11-2a.c
@@ -7,7 +7,7 @@
 
 int main(void) {
   int msqid;      // IPC descriptor for the message queue
-  char pathname[] = "task11_2a.c"; // The file name used to generate the key.
+  char pathname[] = "task11-2a.c"; // The file name used to generate the key.
                              // A file with this name must exist in the current directory.
   key_t  key;     // IPC key
   int i,len;      // Cycle counter and the length of the informative part of the message
